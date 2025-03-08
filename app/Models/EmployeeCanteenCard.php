67d21da7348ad779a3120c565d4dabc160fbcec8
narto <mrzerocrack@gmail.com>
2025-03-09 03:20:23 +0700

    fix sahur

--- app/Models/EmployeeCanteenCard.php
+++ app/Models/EmployeeCanteenCard.php
@@ -10,5 +10,5 @@ class EmployeeCanteenCard extends Model
     use HasFactory;
 
     protected $table = 'employee_canteen_card_masters';
-    protected $fillable = ['employee_id','card_code','status','access_breakfast','access_lunch','access_dinner','notes','created_by','updated_by','created_at','updated_at'];
+    protected $fillable = ['employee_id','card_code','status','access_breakfast','access_lunch','access_dinner','access_suhoor','notes','created_by','updated_by','created_at','updated_at'];
 }
